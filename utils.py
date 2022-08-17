488709beb3d0fef1cdad2358e59b353b055b0976
prateekeserwani <53425691+prateekeserwani@users.noreply.github.com>
2022-08-17 11:34:33 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch
+
+def get_layer_metric_array(net, metric, mode): 
+    metric_array = []
+
+    for layer in net.modules():
+        if mode=='channel' and hasattr(layer,'dont_ch_prune'):
+            continue
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            metric_array.append(metric(layer))
+    return metric_array
+
+
+def get_some_data(train_dataloader, num_batches, device):
+    traindata = []
+    dataloader_iter = iter(train_dataloader)
+    for _ in range(num_batches):
+        traindata.append(next(dataloader_iter))
+    inputs  = torch.cat([a for a,_ in traindata])
+    targets = torch.cat([b for _,b in traindata])
+    inputs = inputs.to(device)
+    targets = targets.to(device)
+    return inputs, targets
